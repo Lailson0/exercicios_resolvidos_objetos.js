a4a477114cf045f1971b7ff2a7d5f2c1d5825d86
lailson <b.lailson0105@gmail.com>
2023-07-19 12:44:47 -0300

    exercício 1 resolvido

---
+++ 01/index.js
@@ -0,0 +1,17 @@
+const endereco = {
+    Rua: "mercês",
+    numero: 50,
+    bairro: "JK",
+    cep: "1234500-123",
+    estado: "BH",
+    país: "Nanana",
+};
+
+const usuario = {
+    nome: "Chico",
+    email: "Chicolaral@...",
+    telefone: "12-1224-2234",
+    data_nascimento: "02/06",
+    endereco
+}
+console.log(usuario);
